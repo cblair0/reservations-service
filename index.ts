99b0fdb74726dbb5b34fe46835c8a345f38ee59f
casey blair <caseyblair@caseys-MacBook-Air.local>
2024-08-22 12:52:32 -0700

    use routes

--- index.ts
+++ index.ts
@@ -3,6 +3,14 @@ import bodyParser from 'body-parser';
 
 const app = express();
 
+import schedulesV1 from './src/routes/api/v1/openings';
+import appointmentsV1 from './src/routes/api/v1/reservations';
+import userV1 from './src/routes/api/v1/users';
+
 app.use(bodyParser.json());
+app.use('/api/v1/schedules', schedulesV1);
+app.use('/api/v1/appointments', appointmentsV1);
+app.use('/api/v1/users', userV1);
+
 
 export default app
